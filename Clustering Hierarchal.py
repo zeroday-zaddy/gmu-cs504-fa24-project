88abb242c72b0b1b7ca2fb3a02e98f24d489bfa1
Vir-gmu <dsingh30@gmu.edu>
2024-11-25 18:46:32 -0500

    Clustering Hierarchal.py

--- Clustering Hierarchal.py
+++ Clustering Hierarchal.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Load the dataset
-data = pd.read_csv('C:/Users/singh/Documents/CS504/Project/Sprint 3/Dataset/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography_20241111.csv')
+data = pd.read_csv('/data/COVID-19_Case_Surveillance_Public_Use_Data_with_Geography_20241111.csv')
 print(f"Initial dataset shape: {data.shape}")
 
 # Selecting relevant features for clustering
